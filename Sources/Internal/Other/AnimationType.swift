6ce83af7eb1029d79aea6a925f953d855b1e4eb9
Criss Myers <criss@missionlabs.co.uk>
2024-04-29 12:42:47 +0100

    update animation

--- Sources/Internal/Other/AnimationType.swift
+++ Sources/Internal/Other/AnimationType.swift
@@ -18,9 +18,15 @@ extension AnimationType {
         case .easeInOut: return .easeInOut(duration: 0.4)
     }}
     var removal: Animation { switch self {
-        case .spring: return .spring(duration: 0.32, bounce: 0, blendDuration: 0.1)
-        case .linear: return .linear(duration: 0.3)
-        case .easeInOut: return .easeInOut(duration: 0.3)
+    case .spring:
+        return if #available(iOS 17, *) {
+            .spring(response: 0.6, dampingFraction: 1, blendDuration: 0.1)
+        } else {
+            .spring(duration: 0.1, bounce: 0, blendDuration: 0.1)
+        }
+        //case .spring: return .spring(duration: 0.32, bounce: 0, blendDuration: 0.1) //original
+    case .linear: return .linear(duration: 0.3)
+    case .easeInOut: return .easeInOut(duration: 0.3)
     }}
     var dragGesture: Animation { .linear(duration: 0.05) }
 }
